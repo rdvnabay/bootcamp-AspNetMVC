c0b6fe3d75c81e28d9a48ea0ff7e11823bf5a342
Rıdvan <ridvanabay@gmail.com>
2021-04-17 17:12:47 +0300

    2.Hafta Cuma (updated)

---
+++ DataAccess/Migrations/202104161837332_InitiaCreate.cs
@@ -0,0 +1,28 @@
+﻿namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitiaCreate : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Contents", "WriterId", "dbo.Writers");
+            DropForeignKey("dbo.Headings", "WriterId", "dbo.Writers");
+            DropIndex("dbo.Headings", new[] { "WriterId" });
+            DropIndex("dbo.Contents", new[] { "WriterId" });
+            DropColumn("dbo.Headings", "WriterId");
+            DropColumn("dbo.Contents", "WriterId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Contents", "WriterId", c => c.Int());
+            AddColumn("dbo.Headings", "WriterId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Contents", "WriterId");
+            CreateIndex("dbo.Headings", "WriterId");
+            AddForeignKey("dbo.Headings", "WriterId", "dbo.Writers", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.Contents", "WriterId", "dbo.Writers", "Id");
+        }
+    }
+}
