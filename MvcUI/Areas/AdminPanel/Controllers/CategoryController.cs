f2ec187a4afd1f8d6717e3c2818c82ef860cf827
Rıdvan <ridvanabay@gmail.com>
2021-05-07 17:41:02 +0300

    5.Hafta - Cuma

---
+++ MvcUI/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Business.Concrete;
+using Business.ValidationRules.FluentValidation;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Areas.AdminPanel.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        // GET: AdminPanel/Category
+        public ActionResult List()
+        {
+            var result = categoryManager.GetAll(); ;
+            return View(result);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(category);
+            if (result.IsValid)
+            {
+                categoryManager.Add(category);
+                return RedirectToAction("List", "Category");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+            }
+            return View(category);
+        }
+    }
+}
